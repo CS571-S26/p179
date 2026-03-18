e83a6675e968597ec27b9ae1f8062396abcfa918
Kate Deviley <kdeviley@wisc.edu>
2026-03-18 16:39:19 -0500

    setup deployment config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/p179",
+  base: "/p179/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
